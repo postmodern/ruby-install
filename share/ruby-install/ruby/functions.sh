6ffa5003acaaf38e0c3e134f0c6c748e8425fb71
Postmodern <postmodern.mod3@gmail.com>
2013-03-31 22:35:50 -0700

    Extract the first two digits from RUBY_VERSION for RUBY_URL.

--- share/ruby-install/ruby/functions.sh
+++ share/ruby-install/ruby/functions.sh
@@ -2,7 +2,7 @@
 
 RUBY_ARCHIVE="ruby-$RUBY_VERSION.tar.gz"
 RUBY_SRC_DIR="ruby-$RUBY_VERSION"
-RUBY_URL="http://ftp.ruby-lang.org/pub/ruby/1.9/$RUBY_ARCHIVE"
+RUBY_URL="http://ftp.ruby-lang.org/pub/ruby/${RUBY_VERSION:0:3}/$RUBY_ARCHIVE"
 
 #
 # Configures Ruby.
