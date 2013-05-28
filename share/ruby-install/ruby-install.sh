338966a25fbd2f7d496c16a342cdf776c3792659
Postmodern <postmodern.mod3@gmail.com>
2013-05-27 23:17:06 -0700

    Moved functions which the build scripts can override into functions.sh.

--- share/ruby-install/ruby-install.sh
+++ share/ruby-install/ruby-install.sh
@@ -159,101 +159,6 @@ function extract()
 }
 
 #
-# Pre-install tasks
-#
-function pre_install()
-{
-	mkdir -p "$SRC_DIR"
-
-	log "Updating Package Manager"
-	update_package_manager
-}
-
-#
-# Install Ruby Dependencies
-#
-function install_deps()
-{
-	local packages=$(fetch dependencies "$PACKAGE_MANAGER")
-
-	if [[ -n "$packages" ]]; then
-		log "Installing dependencies for $RUBY $RUBY_VERSION ..."
-		install_packages $packages
-	fi
-
-	install_optional_deps
-}
-
-#
-# Install any optional dependencies.
-#
-function install_optional_deps() { return; }
-
-#
-# Download the Ruby archive
-#
-function download_ruby()
-{
-	log "Downloading $RUBY_URL into $SRC_DIR ..."
-	download "$RUBY_URL" "$SRC_DIR/$RUBY_ARCHIVE"
-}
-
-#
-# Verifies the Ruby archive matches a checksum.
-#
-function verify_ruby()
-{
-	local md5=$(fetch md5 "$RUBY_ARCHIVE")
-
-	if [[ -n "$md5" ]]; then
-		log "Verifying $RUBY_ARCHIVE ..."
-		verify "$SRC_DIR/$RUBY_ARCHIVE" "$md5"
-	else
-		warn "No checksum for $RUBY_ARCHIVE. Proceeding anyways"
-	fi
-}
-
-#
-# Extract the Ruby archive
-#
-function extract_ruby()
-{
-	log "Extracting $RUBY_ARCHIVE ..."
-	extract "$SRC_DIR/$RUBY_ARCHIVE" "$SRC_DIR"
-}
-
-#
-# Apply any additional patches
-#
-function apply_patches()
-{
-	for path in ${PATCHES[*]}; do
-		log "Applying patch $path ..."
-		patch -p1 < $path
-	done
-}
-
-#
-# Place holder function for configuring Ruby.
-#
-function configure_ruby() { return; }
-
-#
-# Place holder function for compiling Ruby.
-#
-function compile_ruby() { return; }
-
-#
-# Place holder function for installing Ruby.
-#
-function install_ruby() { return; }
-
-#
-# Place holder function for post-install tasks.
-#
-function post_install() { return; }
-
-#
 # Loads function.sh for the given Ruby.
 #
 function load_ruby()
@@ -269,6 +174,7 @@ function load_ruby()
 	RUBY_ARCHIVE="$RUBY-$RUBY_VERSION.tar.gz"
 	RUBY_SRC_DIR="$RUBY-$RUBY_VERSION"
 
+	source "$SHARE_DIR/functions.sh"
 	source "$RUBY_DIR/functions.sh"
 }
 
