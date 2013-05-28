1b49d4976761b6816764778382305062b19f9a8a
Postmodern <postmodern.mod3@gmail.com>
2013-05-27 23:43:42 -0700

    Set SHARE_DIR.

--- test/helper.sh
+++ test/helper.sh
@@ -1,7 +1,10 @@
 [[ -z "$SHUNIT2"     ]] && SHUNIT2=/usr/share/shunit2/shunit2
 [[ -n "$ZSH_VERSION" ]] && setopt shwordsplit
 
+SHARE_DIR="./share/ruby-install"
+
 . ./share/ruby-install/ruby-install.sh
+
 export PATH="$PWD/bin:$PATH"
 
 setUp() { return; }
