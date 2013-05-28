c85d4e3286624906149a719f3bf37c7560486b44
Postmodern <postmodern.mod3@gmail.com>
2013-05-28 16:07:32 -0700

    Updated the homebrew formula for 0.1.1.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyInstall < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
-  url 'https://github.com/postmodern/ruby-install/archive/v0.1.0.tar.gz'
-  sha1 '2f69c5fcc337483b678471d063756b45fded0018'
+  url 'https://github.com/postmodern/ruby-install/archive/v0.1.1.tar.gz'
+  sha1 'f317934f830addcaf6544765a52df03c2d059254'
   head 'https://github.com/postmodern/ruby-install.git'
 
   def install
