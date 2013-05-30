14c0a0f7bb1c8725cafbe0dafb48a61b47655181
Postmodern <postmodern.mod3@gmail.com>
2013-05-29 17:06:24 -0700

    Added tests for download using wget and curl (issue #17).

--- test/download_test.sh
+++ test/download_test.sh
@@ -1,15 +1,29 @@
 . ./test/helper.sh
 
-test_download()
+URL="https://raw.github.com/postmodern/ruby-install/master/README.md"
+OUTPUT="./test/download.txt"
+
+test_download_using_wget()
 {
-	local url="https://raw.github.com/postmodern/ruby-install/master/README.md"
-	local output="./test/download.txt"
+	[[ ! $(type -t wget) ]] && return
 
-	download "$url" "$output" 2>/dev/null
+	DOWNLOADER="wget" download "$URL" "$OUTPUT" 2>/dev/null
 
-	assertTrue "did not download the file" '[[ -f $output ]]'
+	assertTrue "did not download the file" '[[ -f "$OUTPUT" ]]'
+}
 
-	rm "$output"
+test_download_using_curl()
+{
+	[[ ! $(type -t curl ) ]] && return
+
+	DOWNLOADER="curl" download "$URL" "$OUTPUT" 2>/dev/null
+
+	assertTrue "did not download the file" '[[ -f "$OUTPUT" ]]'
+}
+
+tearDown()
+{
+	rm "$OUTPUT"
 }
 
 SHUNIT_PARENT=$0 . $SHUNIT2
