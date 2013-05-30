7afd96aa97f328fcd61eddf75d9533023baf208f
Postmodern <postmodern.mod3@gmail.com>
2013-05-29 22:19:58 -0700

    Add a newline to appease the homebrew style gods.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -4,6 +4,7 @@ class RubyInstall < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
   url 'https://github.com/postmodern/ruby-install/archive/v0.1.1.tar.gz'
   sha1 'f317934f830addcaf6544765a52df03c2d059254'
+
   head 'https://github.com/postmodern/ruby-install.git'
 
   def install
