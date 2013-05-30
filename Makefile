01b9b819496967393f83684bd54785efdde5cfed
Postmodern <postmodern.mod3@gmail.com>
2013-05-30 13:49:53 -0700

    Version bump to 0.1.3.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME=ruby-install
-VERSION=0.1.2
+VERSION=0.1.3
 AUTHOR=postmodern
 URL=https://github.com/$(AUTHOR)/$(NAME)
 
--- share/ruby-install/ruby-install.sh
+++ share/ruby-install/ruby-install.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob
 
-RUBY_INSTALL_VERSION="0.1.2"
+RUBY_INSTALL_VERSION="0.1.3"
 
 RUBIES=(ruby jruby rubinius)
 PATCHES=()
