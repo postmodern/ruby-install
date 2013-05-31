4c732f477369f9b43a85dacd0159d49e61040b60
havenwood <shannonskipper@gmail.com>
2013-05-31 16:48:21 -0700

    add maglev

--- test/supported_rubies_test.sh
+++ test/supported_rubies_test.sh
@@ -7,6 +7,7 @@ test_supported_rubies()
 	assertTrue "did not include ruby" '[[ "$output" == *ruby* ]]'
 	assertTrue "did not include jruby" '[[ "$output" == *jruby* ]]'
 	assertTrue "did not include rubinius" '[[ "$output" == *rubinius* ]]'
+	assertTrue "did not include maglev" '[[ "$output" == *maglev* ]]'
 }
 
 SHUNIT_PARENT=$0 . $SHUNIT2
