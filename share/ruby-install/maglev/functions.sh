571abfd7e0060993c4968f310bad605d2c77937f
havenwood <shannonskipper@gmail.com>
2013-06-01 18:07:51 -0700

    move Gemstone dir from parent dir into Maglev's own dir and symlink

--- share/ruby-install/maglev/functions.sh
+++ share/ruby-install/maglev/functions.sh
@@ -3,8 +3,6 @@
 RUBY_ARCHIVE="MagLev-$RUBY_VERSION.tar.gz"
 RUBY_URL="http://glass-downloads.gemstone.com/maglev/$RUBY_ARCHIVE"
 
-function extract_ruby() { return; }
-
 function configure_ruby() { return; }
 
 function compile_ruby() { return; }
@@ -15,18 +13,26 @@ function compile_ruby() { return; }
 function install_ruby()
 {
 	log "Installing maglev $RUBY_VERSION ..."
-	mkdir "$INSTALL_DIR"
-	tar -xzf "$SRC_DIR/$RUBY_ARCHIVE" -C "$INSTALL_DIR" \
-		                          --strip-components=1
-	cd "$INSTALL_DIR"
-	sh "install.sh"
+	sh "$SRC_DIR/$RUBY_SRC_DIR/install.sh"
+
+	# Determine the Gemstone name with code adapted from Maglev's update.sh.
+	platform="$(uname -sm | tr ' ' '-')"
+	[ $platform="Darwin-x86_64" ] && platform="Darwin-i386"
+	gs_ver=$(grep GEMSTONE "$SRC_DIR/$RUBY_SRC_DIR/version.txt" | cut -f2 -d-)
+	gemstone="GemStone-$gs_ver.$platform"
+
+	# Move the Gemstone from $SRC_DIR to $RUBY_SRC_DIR and symlink gemstone.
+	mv "$SRC_DIR/$gemstone" "$SRC_DIR/$RUBY_SRC_DIR"
+	ln -fs "$gemstone" "$SRC_DIR/$RUBY_SRC_DIR/gemstone"
+
+	mv "$SRC_DIR/$RUBY_SRC_DIR" "$(dirname $INSTALL_DIR)"
 }
 
 #
 # Post-install tasks.
 #
 function post_install()
-{
+{	
 	log "Symlinking bin/ruby to bin/maglev-ruby ..."
 	ln -fs maglev-ruby "$INSTALL_DIR/bin/ruby"
 	
