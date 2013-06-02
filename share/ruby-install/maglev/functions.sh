81b79cda3f8a692fa4222172c645ba870c54ad47
havenwood <shannonskipper@gmail.com>
2013-06-01 18:30:48 -0700

    run install.sh directly and move platform to PLATFORM

--- share/ruby-install/maglev/functions.sh
+++ share/ruby-install/maglev/functions.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+PLATFORM="$(uname -sm | tr ' ' '-')"
+[ $PLATFORM="Darwin-x86_64" ] && PLATFORM="Darwin-i386"
+
 RUBY_ARCHIVE="MagLev-$RUBY_VERSION.tar.gz"
 RUBY_URL="http://glass-downloads.gemstone.com/maglev/$RUBY_ARCHIVE"
 
@@ -13,13 +16,11 @@ function compile_ruby() { return; }
 function install_ruby()
 {
 	log "Installing maglev $RUBY_VERSION ..."
-	sh "$SRC_DIR/$RUBY_SRC_DIR/install.sh"
+	"$SRC_DIR/$RUBY_SRC_DIR/install.sh"
 
 	# Determine the Gemstone name with code adapted from Maglev's update.sh.
-	platform="$(uname -sm | tr ' ' '-')"
-	[ $platform="Darwin-x86_64" ] && platform="Darwin-i386"
 	gs_ver=$(grep GEMSTONE "$SRC_DIR/$RUBY_SRC_DIR/version.txt" | cut -f2 -d-)
-	gemstone="GemStone-$gs_ver.$platform"
+	gemstone="GemStone-$gs_ver.$PLATFORM"
 
 	# Move the Gemstone from $SRC_DIR to $RUBY_SRC_DIR and symlink gemstone.
 	mv "$SRC_DIR/$gemstone" "$SRC_DIR/$RUBY_SRC_DIR"
