89ede34e28ba58fde180e740d2c8af01806c8e04
Postmodern <postmodern.mod3@gmail.com>
2013-06-02 18:37:57 -0700

    Style.

--- share/ruby-install/maglev/functions.sh
+++ share/ruby-install/maglev/functions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PLATFORM="$(uname -sm)"
-[ $PLATFORM="Darwin x86_64" ] && PLATFORM="Darwin i386"
+[[ "$PLATFORM" == "Darwin x86_64" ]] && PLATFORM="Darwin i386"
 
 RUBY_ARCHIVE="MagLev-$RUBY_VERSION.tar.gz"
 RUBY_URL="http://glass-downloads.gemstone.com/maglev/$RUBY_ARCHIVE"
@@ -19,8 +19,8 @@ function install_ruby()
 	"$SRC_DIR/$RUBY_SRC_DIR/install.sh"
 
 	# Determine what Maglev named the Gemstone.
-	gs_ver=$(grep GEMSTONE "$SRC_DIR/$RUBY_SRC_DIR/version.txt")
-	gemstone="GemStone-${gs_ver: -5}.${PLATFORM/ /-}"
+	local gs_ver=$(grep GEMSTONE "$SRC_DIR/$RUBY_SRC_DIR/version.txt")
+	local gemstone="GemStone-${gs_ver: -5}.${PLATFORM/ /-}"
 
 	log "Installing Gemstone into $SRC_DIR/$gemstone ..."
 	ln -fs "$gemstone" "$SRC_DIR/gemstone"
