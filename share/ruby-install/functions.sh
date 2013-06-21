8b0e8c35bbca9117e2a3567809a8ff99cb459178
Postmodern <postmodern.mod3@gmail.com>
2013-06-20 22:44:01 -0700

    Quote more arguments.

--- share/ruby-install/functions.sh
+++ share/ruby-install/functions.sh
@@ -3,7 +3,7 @@
 #
 function check_reinstall()
 {
-	if [[ $NO_REINSTALL && -x "$INSTALL_DIR/bin/ruby" ]]; then
+	if [[ "$NO_REINSTALL" && -x "$INSTALL_DIR/bin/ruby" ]]; then
 		log "Ruby is already installed into $INSTALL_DIR"
 		return 1
 	fi
@@ -15,7 +15,7 @@ function check_reinstall()
 function pre_install()
 {
 	mkdir -p "$SRC_DIR"
-	mkdir -p $(dirname "$INSTALL_DIR")
+	mkdir -p "$(dirname "$INSTALL_DIR")"
 }
 
 #
