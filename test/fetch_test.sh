7de49143fafd3fea411c3e34dbdc7623563f762b
Shannon Skipper <shannonskipper@gmail.com>
2013-06-28 11:10:06 -0700

    Update ruby 1.8 version to ruby-1.8.7-p374.

--- test/fetch_test.sh
+++ test/fetch_test.sh
@@ -8,7 +8,7 @@ setUp()
 test_fetch()
 {
 	local key="1.8.7"
-	local expected="1.8.7-p371"
+	local expected="1.8.7-p374"
 	local value=$(fetch "$RUBY/versions" "$key")
 
 	assertEquals "did not fetch the correct value" "$expected" "$value"
@@ -16,8 +16,8 @@ test_fetch()
 
 test_fetch_with_excess_whitespace()
 {
-	local key="ruby-1.8.7-p371.tar.bz2"
-	local expected="c27526b298659a186bdb5107fcec2341"
+	local key="ruby-1.8.7-p374.tar.bz2"
+	local expected="83c92e2b57ea08f31187060098b2200b"
 	local value=$(fetch "$RUBY/md5" "$key")
 
 	assertEquals "did not fetch the correct value" "$expected" "$value"
