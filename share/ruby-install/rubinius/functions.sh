e3095c08b45ba45e9f1e9f687919b6571e5d2d62
Postmodern <postmodern.mod3@gmail.com>
2013-10-11 21:38:04 -0700

    Only support installing Rubinius >= 2.0.0 (issue #79)
    * Include rubygems as a dependency.
    * Removed rake as a dependency, let bundler install the desired version
      of rake.
    * Install llvm-dev on Debian/Ubuntu systems.
    * Download from http://releases.rubini.us/rubinius-$RUBY_VERSION.tar.bz2.
    * Install bundler from rubygems, to ensure we always have the latest
      version of bundler.
    * Run bundle install before running ./configure.

--- share/ruby-install/rubinius/functions.sh
+++ share/ruby-install/rubinius/functions.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-RUBY_ARCHIVE="rubinius-$RUBY_VERSION.tar.gz"
-RUBY_SRC_DIR="rubinius-release-$RUBY_VERSION"
-RUBY_MIRROR="${RUBY_MIRROR:-https://github.com/rubinius/rubinius/archive}"
-RUBY_URL="${RUBY_URL:-$RUBY_MIRROR/release-$RUBY_VERSION.tar.gz}"
+RUBY_ARCHIVE="rubinius-$RUBY_VERSION.tar.bz2"
+RUBY_SRC_DIR="rubinius-$RUBY_VERSION"
+RUBY_MIRROR="${RUBY_MIRROR:-http://releases.rubini.us}"
+RUBY_URL="${RUBY_URL:-$RUBY_MIRROR/$RUBY_ARCHIVE}"
 
 #
 # Install optional dependencies for Rubinius.
 #
 function install_optional_deps()
 {
-	if [[ "$PACKAGE_MANAGER" == "apt" ]]; then
-		# attempt to install llvm-3.0-dev
-		(sudo apt-get install -y llvm-3.0-dev && sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.0 30) || true
-	fi
+	log "Installing bundler ..."
+	$SUDO gem install bundler
 }
 
 #
@@ -21,8 +19,10 @@ function install_optional_deps()
 #
 function configure_ruby()
 {
-	log "Configuring rubinius $RUBY_VERSION ..."
+	log "Bundling rubinius $RUBY_VERSION ..."
+	$SUDO bundle install
 
+	log "Configuring rubinius $RUBY_VERSION ..."
 	if [[ "$PACKAGE_MANAGER" == "brew" ]]; then
 		./configure --prefix="$INSTALL_DIR" \
 			    --with-opt-dir="$(brew --prefix openssl):$(brew --prefix readline):$(brew --prefix libyaml):$(brew --prefix gdbm)" \
