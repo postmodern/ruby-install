47ebc30ce4cab6dee0e512c51888d8b79ce01614
Postmodern <postmodern.mod3@gmail.com>
2013-11-18 19:25:37 -0800

    Capture the output into a variable.

--- test/no_reinstall_test.sh
+++ test/no_reinstall_test.sh
@@ -1,8 +1,6 @@
 . ./test/helper.sh
 
 INSTALL_DIR="./test/dir"
-STDERR="./test/stderr"
-STDOUT="./test/stdout"
 
 function setUp()
 {
@@ -13,18 +11,16 @@ function setUp()
 
 function test_no_reinstall_when_ruby_executable_exists()
 {
-	ruby-install --install-dir "$INSTALL_DIR" --no-reinstall ruby \
-		> "$STDOUT" 2> "$STDERR"
+	local output="$(ruby-install --install-dir "$INSTALL_DIR" --no-reinstall ruby)"
 
 	assertEquals "did not return 0" 0 $?
 	assertTrue "did not print a message to STDOUT" \
-		'[[ $(cat $STDOUT) == *"already installed"* ]]'
+		'[[ "$output" == *"already installed"* ]]'
 }
 
 function tearDown()
 {
 	rm -r "$INSTALL_DIR"
-	rm -f "$STDERR" "$STDOUT"
 }
 
 SHUNIT_PARENT=$0 . $SHUNIT2
