3b76f1ced2f9a618009e5cf054895c670d885286
Kerem Çakırer <kcakirer@hotmail.com>
2013-12-24 15:59:46 +0200

    Added better bundler detection.
    ruby-install can now detect bundler even if
    it has a suffix. This is useful on distros
    such as openSUSE where the 'format_executable'
    option is enabled by default in '/etc/gemrc'.

--- share/ruby-install/rubinius/functions.sh
+++ share/ruby-install/rubinius/functions.sh
@@ -5,17 +5,40 @@ RUBY_SRC_DIR="rubinius-$RUBY_VERSION"
 RUBY_MIRROR="${RUBY_MIRROR:-http://releases.rubini.us}"
 RUBY_URL="${RUBY_URL:-$RUBY_MIRROR/$RUBY_ARCHIVE}"
 
+RUBY_SUFFIX=`gem env \
+				| grep ' *- *RUBY EXECUTABLE: *.*' \
+				| sed 's/.*: *\(.*\)/\1/' \
+				| sed 's/.*ruby\(.*\)/\1/'`
+BUNDLERS=("bundle" "bundle$RUBY_SUFFIX")
+
+#
+# Check if bundler is installed.
+#
+function bundler_installed()
+{
+	for bundler in ${BUNDLERS[@]}; do
+		command -v "$bundler" >/dev/null && return 0
+	done
+	return 1
+}
+
 #
 # Install optional dependencies for Rubinius.
 #
 function install_optional_deps()
 {
-	if ! command -v bundle >/dev/null; then
+	if ! bundler_installed; then
 		log "Installing bundler ..."
 		if [[ -w "$(gem env gemdir)" ]]; then gem install bundler
 		else                                  sudo gem install bundler
 		fi
 	fi
+	for bundler in ${BUNDLERS[@]}; do
+		command -v "$bundler" >/dev/null && {
+			BUNDLER="$bundler"
+			break
+		}
+	done
 }
 
 #
@@ -24,7 +47,7 @@ function install_optional_deps()
 function configure_ruby()
 {
 	log "Bundling rubinius $RUBY_VERSION ..."
-	bundle install --path vendor/gems
+	"$BUNDLER" install --path vendor/gems
 
 	log "Configuring rubinius $RUBY_VERSION ..."
 	if [[ "$PACKAGE_MANAGER" == "brew" ]]; then
@@ -42,7 +65,7 @@ function configure_ruby()
 function compile_ruby()
 {
 	log "Compiling rubinius $RUBY_VERSION ..."
-	bundle exec rake build
+	"$BUNDLER" exec rake build
 }
 
 #
@@ -51,5 +74,5 @@ function compile_ruby()
 function install_ruby()
 {
 	log "Installing rubinius $RUBY_VERSION ..."
-	bundle exec rake install
+	"$BUNDLER" exec rake install
 }
