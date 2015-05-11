2e339cf57e75a657de0aa26409d8501dda39f435
Postmodern <postmodern.mod3@gmail.com>
2015-05-10 22:13:12 -0700

    Since github doesn't support Last-Modified, re-download if older than an hour.

--- share/ruby-install/cache.sh
+++ share/ruby-install/cache.sh
@@ -1,48 +1,67 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/ruby-install"
-cache_url="https://raw.github.com/postmodern/ruby-versions/master"
+cache_url="https://raw.githubusercontent.com/postmodern/ruby-versions/master"
+
+function cache_is_stale()
+{
+	local file="$cache_dir/$1"
+	local now="$(date +%s)"
+	local one_hour_ago="$(( now - 60 * 60 ))"
+
+	[[ ! -f "$file" ]] || (( $(stat -c %Y "$file") < $one_hour_ago ))
+}
 
 function cache_download()
 {
 	local file="$1"
-	local url="$cache_url/$file"
 
+	download "$cache_url/$file" "$cache_dir/$file" >/dev/null 2>&1 || return $?
+}
 
-	case "$downloader" in
-		wget)
-			wget -q -N -x -nH --cut-dirs 3 -P "$cache_dir" "$url" || return $?
-			;;
-		curl)
-			local path="$cache_dir/$file"
+function cache_delete()
+{
+	local file="$1"
 
-			mkdir -p "${path%/*}"
-			curl -s -f -L -z "$path" -o "$path" "$url" || return $?
-			;;
-		*)
-			error "Could not find wget or curl"
-			return 1
-			;;
-	esac
+	rm -f "$cache_dir/$file"
+}
+
+function cache_update()
+{
+	local file="$1"
+
+	if cache_is_stale "$file"; then
+		cache_delete "$file"   || return $?
+		cache_download "$file" || return $?
+	fi
+}
+
+function cache_update_versions()
+{
+	local ruby="$1"
+
+	cache_update "$ruby/versions.txt" || return $?
+}
+
+function cache_update_stable_versions()
+{
+	local ruby="$1"
+	
+	cache_update "$ruby/stable.txt" || return $?
+}
+
+function cache_update_checksums()
+{
+	local ruby="$1"
+
+	for algorithm in md5 sha1 sha256 sha512; do
+		cache_update "$ruby/checksums.$algorithm" || return $?
+	done
 }
 
 function cache_update_ruby()
 {
 	local ruby="$1"
 
-	if [[ "$ruby" == "rbx" ]]; then
-		ruby="rubinius"
-	fi
-
-	cache_download "$ruby/versions.txt" || return $?
-	cache_download "$ruby/stable.txt"   || return $?
-
-	for algorithm in md5 sha1 sha256 sha512; do
-		cache_download "$ruby/checksums.$algorithm" || return $?
-	done
-}
-
-function cache_update_rubies()
-{
-	for ruby in "${rubies[@]}"; do
-		cache_update_ruby "$ruby" || return $?
-	done
+	cache_update_versions "$ruby"
+	cache_update_stable_versions "$ruby"
+	cache_update_checksums "$ruby"
 }
