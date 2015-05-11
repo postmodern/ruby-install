086b762330e79d944f9b23ffe387becee1ca49c8
Postmodern <postmodern.mod3@gmail.com>
2015-05-11 00:07:52 -0700

    Moved ruby related code out of cache.sh.

--- share/ruby-install/cache.sh
+++ share/ruby-install/cache.sh
@@ -1,5 +1,4 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/ruby-install"
-cache_url="https://raw.githubusercontent.com/postmodern/ruby-versions/master"
 
 function cache_is_stale()
 {
@@ -7,14 +6,15 @@ function cache_is_stale()
 	local now="$(date +%s)"
 	local one_hour_ago="$(( now - 60 * 60 ))"
 
-	[[ ! -f "$file" ]] || (( $(stat -c %Y "$file") < $one_hour_ago ))
+	[[ ! -f "$file" ]] || [[ -n "$(find "$file" -mmin +60)" ]]
 }
 
 function cache_download()
 {
 	local file="$1"
+	local base_url="$2"
 
-	download "$cache_url/$file" "$cache_dir/$file" >/dev/null 2>&1 || return $?
+	download "$base_url/$file" "$cache_dir/$file" >/dev/null 2>&1 || return $?
 }
 
 function cache_delete()
@@ -27,41 +27,8 @@ function cache_delete()
 function cache_update()
 {
 	local file="$1"
+	local base_url="$2"
 
-	if cache_is_stale "$file"; then
-		cache_delete "$file"   || return $?
-		cache_download "$file" || return $?
-	fi
-}
-
-function cache_update_versions()
-{
-	local ruby="$1"
-
-	cache_update "$ruby/versions.txt" || return $?
-}
-
-function cache_update_stable_versions()
-{
-	local ruby="$1"
-	
-	cache_update "$ruby/stable.txt" || return $?
-}
-
-function cache_update_checksums()
-{
-	local ruby="$1"
-
-	for algorithm in md5 sha1 sha256 sha512; do
-		cache_update "$ruby/checksums.$algorithm" || return $?
-	done
-}
-
-function cache_update_ruby()
-{
-	local ruby="$1"
-
-	cache_update_versions "$ruby"
-	cache_update_stable_versions "$ruby"
-	cache_update_checksums "$ruby"
+	cache_delete "$file"   || return $?
+	cache_download "$file" "$base_url" || return $?
 }
