e17d673f9d0bb60ae9905ca718ef10eb0e82c117
Postmodern <postmodern.mod3@gmail.com>
2015-05-17 18:35:19 -0700

    Override $HOME to avoid touching the actual $HOME.

--- test/helper.sh
+++ test/helper.sh
@@ -1,10 +1,13 @@
 [[ -z "$SHUNIT2"     ]] && SHUNIT2=/usr/share/shunit2/shunit2
 [[ -n "$ZSH_VERSION" ]] && setopt shwordsplit
 
+export HOME="$PWD/test/home"
+export PATH="$PWD/bin:$PATH"
+
+mkdir -p "$HOME"
+
 . ./share/ruby-install/ruby-install.sh
 . ./share/ruby-install/functions.sh
-
-export PATH="$PWD/bin:$PATH"
 
 function setUp() { return; }
 function tearDown() { return; }
