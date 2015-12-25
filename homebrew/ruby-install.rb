766c06325a7f3bc22f624b35690c1f765ef974a3
Postmodern <postmodern.mod3@gmail.com>
2015-12-25 02:36:33 -0800

    Use homebrew style syntax.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -1,12 +1,12 @@
 class RubyInstall < Formula
   desc "Install Ruby, JRuby, Rubinius, MagLev, or mruby"
-  homepage 'https://github.com/postmodern/ruby-install#readme'
-  url 'https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz'
-  sha1 'ee2f316ab5682ad8f89d91b88d57354ea5f2253c'
+  homepage "https://github.com/postmodern/ruby-install#readme"
+  url "https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz"
+  sha1 "ee2f316ab5682ad8f89d91b88d57354ea5f2253c"
 
-  head 'https://github.com/postmodern/ruby-install.git'
+  head "https://github.com/postmodern/ruby-install.git"
 
   def install
-    system 'make', 'install', "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 end
