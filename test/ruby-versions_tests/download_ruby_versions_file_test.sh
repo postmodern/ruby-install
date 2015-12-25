86bc539d8242c8923c8fb7020d37cf6f884d19b5
Postmodern <postmodern.mod3@gmail.com>
2015-12-24 20:40:42 -0800

    Added tests for download_ruby_versions_file.

---
+++ test/ruby-versions_tests/download_ruby_versions_file_test.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+. ./test/helper.sh
+. ./share/ruby-install/ruby-versions.sh
+
+ruby="ruby"
+file="stable.txt"
+expected_path="$ruby_install_cache_dir/$ruby/$file"
+
+function test_download_ruby_versions_file_with_no_parent_dir()
+{
+	rm -rf "$ruby_install_cache_dir/$ruby"
+
+	download_ruby_versions_file "$ruby" "$file"
+
+	assertTrue "did not create the parent dir" \
+		   '[[ -d "$ruby_install_cache_dir/$ruby" ]]'
+}
+
+function test_download_ruby_versions_file_first_time()
+{
+	mkdir -p "$ruby_install_cache_dir/$ruby"
+
+	download_ruby_versions_file "$ruby" "$file"
+
+	assertTrue "did not create the file" \
+		   '[[ -f "$expected_path" ]]'
+	assertTrue "did not write data to the file" \
+		   '[[ -s "$expected_path" ]]'
+}
+
+function test_download_ruby_versions_file_with_existing_file()
+{
+	mkdir -p "$ruby_install_cache_dir/$ruby"
+	touch "$ruby_install_cache_dir/$ruby/$file"
+
+	download_ruby_versions_file "$ruby" "$file"
+
+	assertTrue "did not write data to the file" \
+		   '[[ -s "$expected_path" ]]'
+}
+
+function tearDown()
+{
+	rm -rf "$ruby_install_cache_dir/$ruby"
+}
+
+SHUNIT_PARENT=$0 . $SHUNIT2
