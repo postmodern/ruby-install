75324630b043a263d451e273a3bf3b4ddacabc59
Postmodern <postmodern.mod3@gmail.com>
2015-12-25 02:57:19 -0800

    Switch homebrew formula to use SHA256.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -2,7 +2,7 @@ class RubyInstall < Formula
   desc "Install Ruby, JRuby, Rubinius, MagLev, or mruby"
   homepage "https://github.com/postmodern/ruby-install#readme"
   url "https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz"
-  sha1 "ee2f316ab5682ad8f89d91b88d57354ea5f2253c"
+  sha256 "3cc90846ca972d88b601789af2ad9ed0a496447a13cb986a3d74a4de062af37d"
 
   head "https://github.com/postmodern/ruby-install.git"
 
