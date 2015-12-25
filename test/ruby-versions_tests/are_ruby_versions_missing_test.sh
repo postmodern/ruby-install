ee429f449412a09cdbe1375f200be365d86b0fe8
Postmodern <postmodern.mod3@gmail.com>
2015-12-24 22:56:45 -0800

    Use mkdir -p

--- test/ruby-versions_tests/are_ruby_versions_missing_test.sh
+++ test/ruby-versions_tests/are_ruby_versions_missing_test.sh
@@ -19,7 +19,7 @@ function test_are_ruby_versions_missing_with_no_parent_dir()
 
 function test_are_ruby_versions_missing_with_no_ruby_dir()
 {
-	mkdir "$ruby_install_cache_dir"
+	mkdir -p "$ruby_install_cache_dir"
 
 	are_ruby_versions_missing "$ruby"
 
@@ -28,7 +28,7 @@ function test_are_ruby_versions_missing_with_no_ruby_dir()
 
 function test_are_ruby_versions_missing_with_no_files()
 {
-	mkdir "$ruby_install_cache_dir/$ruby"
+	mkdir -p "$ruby_install_cache_dir/$ruby"
 
 	are_ruby_versions_missing "$ruby"
 
@@ -37,7 +37,7 @@ function test_are_ruby_versions_missing_with_no_files()
 
 function test_are_ruby_versions_missing_with_some_files()
 {
-	mkdir "$ruby_install_cache_dir/$ruby"
+	mkdir -p "$ruby_install_cache_dir/$ruby"
 	touch "$ruby_install_cache_dir/$ruby/stable.txt"
 
 	are_ruby_versions_missing "$ruby"
@@ -47,7 +47,7 @@ function test_are_ruby_versions_missing_with_some_files()
 
 function test_are_ruby_versions_missing_with_all_files()
 {
-	mkdir "$ruby_install_cache_dir/$ruby"
+	mkdir -p "$ruby_install_cache_dir/$ruby"
 
 	for file in "${ruby_versions_files[@]}"; do
 		touch "$ruby_install_cache_dir/$ruby/$file"
