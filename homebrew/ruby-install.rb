17c559fe43eee17a4664d84ef0d4e102e9fde5e9
Postmodern <postmodern.mod3@gmail.com>
2015-12-25 02:31:35 -0800

    Mention mruby in the homebrew formula desc.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -1,5 +1,5 @@
 class RubyInstall < Formula
-  desc "Install Ruby, JRuby, Rubinius, or MagLev"
+  desc "Install Ruby, JRuby, Rubinius, MagLev, or mruby"
   homepage 'https://github.com/postmodern/ruby-install#readme'
   url 'https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz'
   sha1 'ee2f316ab5682ad8f89d91b88d57354ea5f2253c'
