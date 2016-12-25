00ec1806fc2e9a89d0e1198aacae91cba0ae3de8
Postmodern <postmodern.mod3@gmail.com>
2016-12-24 20:36:52 -0800

    Updated the homebrew formula for 0.6.1.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -1,8 +1,8 @@
 class RubyInstall < Formula
   desc "Install Ruby, JRuby, Rubinius, MagLev, or mruby"
   homepage "https://github.com/postmodern/ruby-install#readme"
-  url "https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz"
-  sha256 "3cc90846ca972d88b601789af2ad9ed0a496447a13cb986a3d74a4de062af37d"
+  url "https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz"
+  sha256 "b3adf199f8cd8f8d4a6176ab605db9ddd8521df8dbb2212f58f7b8273ed85e73"
 
   head "https://github.com/postmodern/ruby-install.git"
 
