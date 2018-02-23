4168425f0f77742c51d84d0e4c05b8271f6f67ca
Postmodern <postmodern.mod3@gmail.com>
2018-02-22 17:30:13 -0800

    Moved package manager code into a separate file.

--- share/ruby-install/util.sh
+++ share/ruby-install/util.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-#
-# Auto-detect the package manager.
-#
-if   command -v zypper  >/dev/null; then package_manager="zypper"
-elif command -v apt-get >/dev/null; then package_manager="apt"
-elif command -v dnf     >/dev/null; then package_manager="dnf"
-elif command -v yum     >/dev/null; then package_manager="yum"
-elif command -v pkg     >/dev/null; then package_manager="pkg"
-elif command -v port    >/dev/null; then package_manager="port"
-elif command -v brew    >/dev/null; then package_manager="brew"
-elif command -v pacman  >/dev/null; then package_manager="pacman"
-fi
+source "$ruby_install_dir/package_manager.sh"
 
 #
 # Auto-detect the downloader.
@@ -87,30 +76,6 @@ function fetch()
 			echo "${line##$key:*([[:space:]])}"
 		fi
 	done < "$file"
-}
-
-function install_packages()
-{
-	case "$package_manager" in
-		apt)	$sudo apt-get install -y "$@" || return $? ;;
-		dnf|yum)$sudo $package_manager install -y "$@" || return $?     ;;
-		port)   $sudo port install "$@" || return $?       ;;
-		pkg)	$sudo pkg install -y "$@" || return $?     ;;
-		brew)
-			local brew_owner="$(/usr/bin/stat -f %Su "$(command -v brew)")"
-			sudo -u "$brew_owner" brew install "$@" ||
-			sudo -u "$brew_owner" brew upgrade "$@" || return $?
-			;;
-		pacman)
-			local missing_pkgs=($(pacman -T "$@"))
-
-			if (( ${#missing_pkgs[@]} > 0 )); then
-				$sudo pacman -S "${missing_pkgs[@]}" || return $?
-			fi
-			;;
-		zypper) $sudo zypper -n in -l $* || return $? ;;
-		"")	warn "Could not determine Package Manager. Proceeding anyway." ;;
-	esac
 }
 
 #
