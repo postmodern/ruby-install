ec7d7d70a40609218d0ca24957546cd6e85d2b21
Johann Fuechsl <johann@fuechsl.co>
2019-10-24 09:39:33 +0200

    Create rubyjemalloc variant

---
+++ share/ruby-install/rubyjemalloc/functions.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+ruby_version_family="${ruby_version:0:3}"
+ruby_archive="ruby-$ruby_version.tar.bz2"
+ruby_dir_name="rubyjemalloc-$ruby_version"
+ruby_mirror="${ruby_mirror:-https://cache.ruby-lang.org/pub/ruby}"
+ruby_url="${ruby_url:-$ruby_mirror/$ruby_version_family/$ruby_archive}"
+
+#
+# Extract the Ruby archive
+#
+function extract_ruby()
+{
+	log "Extracting $ruby_archive to $src_dir/$ruby_dir_name ..."
+	mkdir -p "$src_dir/$ruby_dir_name" || return $?
+	extract "$src_dir/$ruby_archive" "$src_dir/$ruby_dir_name" --strip-components=1 || return $?
+}
+
+#
+# Configures Ruby.
+#
+function configure_ruby()
+{
+	if [[ ! -s configure || configure.in -nt configure ]]; then
+		log "Regenerating ./configure script ..."
+		autoreconf || return $?
+	fi
+
+	local opt_dir
+
+	log "Configuring ruby $ruby_version ..."
+	case "$package_manager" in
+		brew)
+			opt_dir="$(brew --prefix openssl):$(brew --prefix readline):$(brew --prefix libyaml):$(brew --prefix gdbm)"
+			;;
+		port)
+			opt_dir="/opt/local"
+			;;
+	esac
+
+	./configure --prefix="$install_dir" \
+		    "${opt_dir:+--with-opt-dir="$opt_dir"}" \
+			--with-jemalloc \
+		    "${configure_opts[@]}" || return $?
+}
+
+#
+# Cleans Ruby.
+#
+function clean_ruby()
+{
+	log "Cleaning ruby $ruby_version ..."
+	make clean || return $?
+}
+
+#
+# Compiles Ruby.
+#
+function compile_ruby()
+{
+	log "Compiling ruby $ruby_version ..."
+	make "${make_opts[@]}" || return $?
+}
+
+#
+# Installs Ruby into $install_dir
+#
+function install_ruby()
+{
+	log "Installing ruby $ruby_version ..."
+	make install || return $?
+}
