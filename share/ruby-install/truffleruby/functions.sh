55c7f5122f3d9e094af3585bee45083f4f29e57a
Benoit Daloze <eregontp@gmail.com>
2020-02-14 00:11:02 +0100

    Add support for downloading truffleruby-head
    * Uses the latest nightly, which is automatically built.

--- share/ruby-install/truffleruby/functions.sh
+++ share/ruby-install/truffleruby/functions.sh
@@ -3,10 +3,22 @@
 platform=$(platform) || return $?
 arch=$(architecture) || return $?
 
-ruby_dir_name="truffleruby-$ruby_version-$platform-$arch"
-ruby_archive="$ruby_dir_name.tar.gz"
-ruby_mirror="${ruby_mirror:-https://github.com/oracle/truffleruby/releases/download}"
-ruby_url="${ruby_url:-$ruby_mirror/vm-$ruby_version/$ruby_archive}"
+if [[ "$ruby_version" == "head" ]]; then
+	local head_platform
+	case "$platform" in
+		linux) head_platform="ubuntu-18.04" ;;
+		macos) head_platform="macos-latest" ;;
+	esac
+	ruby_dir_name="truffleruby-head"
+	ruby_archive="truffleruby-head-$head_platform.tar.gz"
+	ruby_mirror="${ruby_mirror:-https://github.com/ruby/truffleruby-dev-builder/releases/latest/download}"
+	ruby_url="${ruby_url:-$ruby_mirror/$ruby_archive}"
+else
+	ruby_dir_name="truffleruby-$ruby_version-$platform-$arch"
+	ruby_archive="$ruby_dir_name.tar.gz"
+	ruby_mirror="${ruby_mirror:-https://github.com/oracle/truffleruby/releases/download}"
+	ruby_url="${ruby_url:-$ruby_mirror/vm-$ruby_version/$ruby_archive}"
+fi
 
 #
 # Install TruffleRuby into $install_dir.
