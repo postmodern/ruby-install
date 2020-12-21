e2b266e608d1e7b0cbb215b76c0f82120dcab551
Postmodern <postmodern.mod3@gmail.com>
2020-12-20 17:50:53 -0800

    Updated the homebrew recipe for 0.8.1.

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -1,8 +1,8 @@
 class RubyInstall < Formula
   desc "Install Ruby, JRuby, Rubinius, TruffleRuby, or mruby"
   homepage "https://github.com/postmodern/ruby-install#readme"
-  url "https://github.com/postmodern/ruby-install/archive/v0.8.0.tar.gz"
-  sha256 "1b8614997201eb634a88039cc8da947a4af1ee338aaa8fbade623d6748cb0c30"
+  url "https://github.com/postmodern/ruby-install/archive/v0.8.1.tar.gz"
+  sha256 "d96fce7a4df70ca7a367400fbe035ff5b518408fc55924966743abf66ead7771"
   license "MIT"
   head "https://github.com/postmodern/ruby-install.git"
 
