ca52936fe48955c91080d0981e44ce04776783cd
Kattouf <vasiliy.kattouf@gmail.com>
2024-06-14 10:38:52 +0700

    install xz as regular dependency

--- homebrew/ruby-install.rb
+++ homebrew/ruby-install.rb
@@ -6,8 +6,6 @@ class RubyInstall < Formula
   license "MIT"
   head "https://github.com/postmodern/ruby-install.git", branch: "master"
 
-  depends_on 'xz'
-
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "d19d4a89f08d447d522387e362b5e4627de5438e06396e2035fe735dc192d7de"
     sha256 cellar: :any_skip_relocation, big_sur:       "cd1ffd6780ab70d0701e812784ed7a7bb2276c6fc8f3303570fbf200a4f2ccde"
