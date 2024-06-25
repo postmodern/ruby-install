cac5975f7d595447b4c4e28cda2a281e637abb04
Postmodern <postmodern.mod3@gmail.com>
2024-06-25 14:05:49 -0700

    Drop `gdbm` as a dependency for ruby >= 3.1.0 (closes #483).

--- test/functions-tests/load_dependencies_test.sh
+++ test/functions-tests/load_dependencies_test.sh
@@ -15,7 +15,8 @@ function test_load_dependencies()
 	load_dependencies
 
 	assertEquals "did not correctly set \$ruby_dependencies from \$ruby/dependencies.sh for \$package_manager" \
-		     "${ruby_dependencies[*]}" "xz gcc automake bison zlib-devel libyaml-devel openssl-devel gdbm-devel readline-devel ncurses-devel libffi-devel"
+		     "xz gcc automake bison zlib-devel libyaml-devel openssl-devel readline-devel ncurses-devel libffi-devel gdbm-devel" \
+		     "${ruby_dependencies[*]}"
 }
 
 function tearDown()
