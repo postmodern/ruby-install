3b86749e128ffac17af1f71ec740239862f94cf4
Postmodern <postmodern.mod3@gmail.com>
2025-01-31 13:36:26 -0800

    Updated the dependencies for JRuby (closes #498).
    * Update to OpenJDK 21.
    * Use JRE specific packages when possible.

--- share/ruby-install/jruby/dependencies.sh
+++ share/ruby-install/jruby/dependencies.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 case "$package_manager" in
-	apt) 	ruby_dependencies=(openjdk-8-jdk) ;;
-	dnf|yum)ruby_dependencies=(java-openjdk) ;;
-	pacman) ruby_dependencies=(jre8-openjdk) ;;
-	zypper) ruby_dependencies=(java-1.8.0-openjdk) ;;
-	pkg) 	ruby_dependencies=(openjdk) ;;
-	xbps) 	ruby_dependencies=(openjdk) ;;
+	apt) 	ruby_dependencies=(openjdk-21-jre) ;;
+	dnf|yum)ruby_dependencies=(java-21-openjdk) ;;
+	pacman) ruby_dependencies=(jre21-openjdk) ;;
+	zypper) ruby_dependencies=(java-21-openjdk) ;;
+	pkg) 	ruby_dependencies=(openjdk21-jre) ;;
+	xbps) 	ruby_dependencies=(openjdk-jre) ;;
 esac
