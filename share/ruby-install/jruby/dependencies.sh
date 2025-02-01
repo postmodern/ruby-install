e256ad5ef0cfbe36442b77175de38d7111bbb002
Postmodern <postmodern.mod3@gmail.com>
2025-02-01 01:53:49 -0800

    Use `default-jre` (apt) and `java-latest-openjdk` (dnf/yum).
    * This should always install the latest version of OpenJDK.

--- share/ruby-install/jruby/dependencies.sh
+++ share/ruby-install/jruby/dependencies.sh
@@ -5,8 +5,8 @@ ruby_dependencies=()
 # NOTE: only install OpenJDK JRE if java isn't already installed.
 if ! command -v java >/dev/null; then
 	case "$package_manager" in
-		apt) 	ruby_dependencies+=(openjdk-21-jre) ;;
-		dnf|yum)ruby_dependencies+=(java-21-openjdk) ;;
+		apt) 	ruby_dependencies+=(default-jre) ;;
+		dnf|yum)ruby_dependencies+=(java-latest-openjdk) ;;
 		pacman) ruby_dependencies+=(jre21-openjdk) ;;
 		zypper) ruby_dependencies+=(java-21-openjdk) ;;
 		pkg) 	ruby_dependencies+=(openjdk21-jre) ;;
