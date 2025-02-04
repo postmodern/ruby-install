3e0483ae4fd36fc98f431eb0f47b88b8a11ead53
Postmodern <postmodern.mod3@gmail.com>
2025-02-03 16:43:33 -0800

    Fixed how shunit2 is detected on FreeBSD and macOS.

--- test/helper.sh
+++ test/helper.sh
@@ -1,8 +1,8 @@
 if [[ -z "$SHUNIT2" ]]; then
 	if [[ -f /usr/share/shunit2/shunit2 ]]; then
 		SHUNIT2=/usr/share/shunit2/shunit2
-	elif [[ -f /usr/bin/shunit2 ]]; then
-		SHUNIT2=/usr/bin/shunit2
+	elif shunit2="$(command -v shunit2)"
+		SHUNIT2="$shunit2"
 	else
 		echo "$0: shunit2 is not installed." >&2
 		exit 1
